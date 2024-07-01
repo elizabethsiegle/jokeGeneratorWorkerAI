83a723a5b332018726ed4310ba67e99f12323e29
Lizzie Siegle <esiegle@brynmawr.edu>
2024-07-01 13:29:34 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+import html from '../static/index.html';
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+    async fetch(request, env) {
+        const url = new URL(request.url);
+
+        if (request.method === 'GET' && url.pathname === '/') {
+            return new Response(html, {
+                headers: { 'Content-Type': 'text/html' },
+            });
+        } else if (request.method === 'POST' && url.pathname === '/joke') {
+            const { name, favoriteFruit, favoriteColor, random, model } = await request.json();
+            const messages = [
+				{ role: "system", content: "You are a friendly assistant" },
+				{
+				  role: "user",
+				  content: `Return nothing else besides a joke for someone named ${name} whose favorite color is ${favoriteColor} and favorite fruit is ${favoriteFruit}. They also say ${random}.`,
+				},
+			  ];
+			console.log(name, favoriteColor, favoriteFruit, random, model);
+            const response = await env.AI.run(
+                model,
+                { messages }
+              );
+            console.log(JSON.stringify(response["response"]));
+            return new Response(JSON.stringify(response), {
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        return new Response('Not found', { status: 404 });
+    }
+}
